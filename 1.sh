1ae1f13a03d9a63b45711072638be87901e385e1
Sadjin13 <150779680+Sadjin13@users.noreply.github.com>
2024-10-02 13:12:52 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,5 +3,5 @@ docker stop nextcloud &&
 	echo stoped || echo Erorr
 docker rm nextcloud &&
 	echo delete || echo Erorr
-docker run -d -p 9999:80 --name nextcloud -v /home/sadjin/nextcloud/:/var/www/html nextcloud &&
+docker run -d -p 9999:80 --name nextcloud -v /home/sadjin/nextcloud/:/var/www/html nextcloud:moe &&
 	echo let go || echo Erorr
